44471c7510e86c62fae1a984c7c28ec9c23932be
Priyanka Sahu <110932339+CodeWithSony@users.noreply.github.com>
2023-03-08 23:06:43 +0530

    Initial commit

---
+++ holihai/index.js
@@ -0,0 +1,28 @@
+var audio = document.createElement("audio");
+audio.setAttribute("src", "music.mp3");
+audio.loop = true;
+muted = "muted";
+// audio.play();
+
+
+
+
+let girl = document.querySelector('#girl');
+let waterGun = document.querySelector('.water-gun')
+let container = document.querySelector('.container')
+let boy = document.querySelector('.boy')
+girl.addEventListener("click", function hello() {
+
+  waterGun.classList.toggle('rotateItt');
+  container.classList.toggle('redBricksBg')
+
+});
+
+boy.addEventListener("click", function hello() {
+
+waterGun.classList.toggle('rotateIt');
+
+container.classList.toggle('colorBg');
+
+});
+
